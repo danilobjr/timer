b1905c26e9e099910700d2fa6ce0cfdf282c26a4
Danilo Barros <danilobjr@gmail.com>
2016-06-02 16:53:15 -0300

    fix main class name of NavigationBar

--- src/components/common/Navigation/NavigationBar.jsx
+++ src/components/common/Navigation/NavigationBar.jsx
@@ -4,7 +4,7 @@ import { NavigationBarItem } from './NavigationBarItem';
 export class NavigationBar extends Component {    
     render() {
         return (
-            <div className="command-bar">
+            <div className="navigation-bar">
                 <ul>
                     <NavigationBarItem href="/timer" text="Timer" />
                     <NavigationBarItem href="/chronometer" text="Chronometer" />
