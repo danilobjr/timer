c65e711536be6c5843bad8533a7afd63646eba11
Danilo Barros <danilobjr@gmail.com>
2016-06-03 00:45:55 -0300

    action for NavigationBar

---
+++ src/components/common/NavigationBar/actions.js
@@ -0,0 +1,6 @@
+export const CHANGE_ACTIVE_NAVIGATION_BAR_ITEM = 'CHANGE_ACTIVE_NAVIGATION_BAR_ITEM';
+
+export const changeActiveNavigationBarItem = (text) => ({
+    type: CHANGE_ACTIVE_NAVIGATION_BAR_ITEM,
+    text
+})
