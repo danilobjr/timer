f2120fa5708d5ed01ebd3372a89cf9a6ac5935b3
Danilo Barros <danilobjr@gmail.com>
2016-06-03 15:51:21 -0300

    navigation items

---
+++ src/components/common/NavigationBar/items.js
@@ -0,0 +1,12 @@
+export const items = [
+    {
+        text: 'Timer',
+        href: '/timer',
+        iconClassName: 'ion-md-time'
+    },
+    {
+        text: 'Chronometer',
+        href: '/chronometer',
+        iconClassName: 'ion-md-stopwatch'
+    }
+];
