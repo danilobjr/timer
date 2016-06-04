2f6797abce1e242f00f4e8862dcfc9492ba5802c
Danilo Barros <danilobjr@gmail.com>
2016-06-04 14:17:50 -0300

    basic markup for new Timer/Counter component

--- src/components/Timer/Timer.jsx
+++ src/components/Timer/Timer.jsx
@@ -7,16 +7,44 @@ import {
 
 export class Timer extends Component {
     render() {
+        // back use for FlexBox n FlexItem when done with new Timer/Counter component
         return (
-            <FlexBox column>
+            <div>
                 <NavigationBar />
-                <FlexItem grow={1}>Timer content</FlexItem>
+                <div style={{display: 'flex', justifyContent: 'center', paddingTop: '12px'}}>
+                    <div className="timer">
+                        <div className="counter">
+                            <span className="unit">00</span>
+                            <span className="unit active">21</span>
+                            <span className="unit active">40</span>
+                        </div>
+                        <div className="commands">
+                            <button className="reset">
+                                <span className="icon ion-md-refresh"></span>
+                            </button>
+                            <button className="start">
+                                <span className="icon ion-md-play"></span>
+                            </button>
+                            <button className="expand">
+                                <span className="icon ion-md-expand"></span>
+                            </button>
+                        </div>
+                        <div className="info">
+                            <span className="name">My interval</span>
+                            <div className="counter">
+                                <span className="unit">00</span>
+                                <span className="unit">21</span>
+                                <span className="unit">40</span>
+                            </div>
+                        </div>
+                    </div>
+                </div>
                 <CommandBar>
                     <CommandBarItem icon="ion-md-add" />
                     <CommandBarItem icon="ion-md-trash" />
                     <CommandBarItem icon="ion-md-more" />
                 </CommandBar>
-            </FlexBox>
+            </div>
         );
     }
 }
