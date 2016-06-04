313c163bd2904c7e0d5f036b20c74449a1fa72a5
Danilo Barros <danilobjr@gmail.com>
2016-06-04 20:54:18 -0300

    refatoring on TImerPage for use Timer component

--- src/components/TimerPage/TimerPage.jsx
+++ src/components/TimerPage/TimerPage.jsx
@@ -2,7 +2,8 @@ import React, { Component } from 'react';
 import { 
     CommandBar, CommandBarItem, 
     FlexBox, FlexItem,
-    NavigationBar 
+    NavigationBar,
+    Timer
 } from 'components/common';
 
 export class TimerPage extends Component {
@@ -12,32 +13,7 @@ export class TimerPage extends Component {
             <div>
                 <NavigationBar />
                 <div style={{display: 'flex', justifyContent: 'center', paddingTop: '12px'}}>
-                    <div className="timer">
-                        <div className="counter">
-                            <span className="unit">00</span>
-                            <span className="unit active">21</span>
-                            <span className="unit active">40</span>
-                        </div>
-                        <div className="commands">
-                            <button className="reset">
-                                <span className="icon ion-md-refresh"></span>
-                            </button>
-                            <button className="start">
-                                <span className="icon ion-md-play"></span>
-                            </button>
-                            <button className="expand">
-                                <span className="icon ion-md-expand"></span>
-                            </button>
-                        </div>
-                        <div className="info">
-                            <span className="name">My interval</span>
-                            <div className="counter">
-                                <span className="unit">00</span>
-                                <span className="unit">21</span>
-                                <span className="unit">40</span>
-                            </div>
-                        </div>
-                    </div>
+                    <Timer />
                 </div>
                 <CommandBar>
                     <CommandBarItem icon="ion-md-add" />
