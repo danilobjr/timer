a92fb63ded69ba7df0b0bc6da4cca28cc2d022fa
Danilo Barros <danilobjr@gmail.com>
2016-06-05 20:25:13 -0300

    passing props to main element

--- src/components/common/CommandBar/CommandBarItem.jsx
+++ src/components/common/CommandBar/CommandBarItem.jsx
@@ -3,7 +3,7 @@ import React, { Component, PropTypes } from 'react';
 const renderHref = (href) => href ? `#${href}` : '#' 
 
 export const CommandBarItem = (props) =>
-    <a className="command-bar-item" href={renderHref(props.href)}>
+    <a {...props} className="command-bar-item" href={renderHref(props.href)}>
         <span className={`icon ${props.icon}`}></span>
     </a>
 
