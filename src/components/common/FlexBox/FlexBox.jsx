96fcb38029a601e6c0257d3340204baa006ddf59
Danilo Barros <danilobjr@gmail.com>
2016-06-05 20:02:11 -0300

    new justify prop n getting external className prop

--- src/components/common/FlexBox/FlexBox.jsx
+++ src/components/common/FlexBox/FlexBox.jsx
@@ -2,13 +2,20 @@ import React, { Component, PropTypes } from 'react';
 
 const renderClassNames = (props) => {
     return [
+        props.className,
         'flex-box',
-        props.column && '-column'
+        props.column && '-column',
+        `-${props.justify}`
     ].join(' ');
 }
 
 export const FlexBox = (props) => <div className={renderClassNames(props)}>{props.children}</div>
 
 FlexBox.propTypes = {
-    column: PropTypes.bool
+    column: PropTypes.bool,
+    justify: PropTypes.oneOf(['flex-start', 'flex-end', 'center', 'space-around', 'space-between'])
+};
+
+FlexBox.defaultProps = {
+    justify: 'flex-start'
 };
