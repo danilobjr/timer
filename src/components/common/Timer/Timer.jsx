69566c7afad6f870636a6f64438c127d3d66f63a
Danilo Barros <danilobjr@gmail.com>
2016-06-05 14:30:02 -0300

    Timer starts n stops count

--- src/components/common/Timer/Timer.jsx
+++ src/components/common/Timer/Timer.jsx
@@ -1,26 +1,84 @@
-import React, { Component } from 'react';
+import React, { Component, PropTypes } from 'react';
 import { CounterWatch } from './CounterWatch';
 import { StartPauseButton } from './StartPauseButton';
 
 export class Timer extends Component {
+    constructor(props) {
+        super(props);
+    
+        this.state = {
+            paused: true,
+            counter: 0
+        };
+        
+        this.interval = null;
+    }
+    
+    componentDidMount() {
+        const oneSecond = 1000;
+        
+        this.interval = setInterval(() => {
+            const { paused, counter } = this.state;
+            
+            if (!paused) {
+                this.setState({ counter: counter + oneSecond });
+            }
+        }, oneSecond);
+    }
+    
+    componentWillUnmount() {
+        clearInterval(this.interval);
+    }
+    
     render() {
+        const { paused, counter } = this.state;
+        const { name, time } = this.props;
+        
         return (
             <div className="timer">
-                <CounterWatch />
+                <CounterWatch currentTime={counter} totalTime={time} />
                 <div className="commands">
                     <button className="reset">
                         <span className="icon ion-md-refresh"></span>
                     </button>
-                    <StartPauseButton isPlaying percentageProgress={.5} />
+                    <StartPauseButton 
+                        showPause={!paused}
+                        percentageProgress={this.calculatePercentageProgress()}
+                        onClick={this.togglePaused.bind(this)}
+                    />
                     <button className="expand">
                         <span className="icon ion-md-expand"></span>
                     </button>
                 </div>
                 <div className="info">
-                    <span className="name">My interval</span>
-                    <CounterWatch />
+                    <span className="name">{name}</span>
+                    <CounterWatch totalTime={time} />
                 </div>
             </div>
         );
     }
+    
+    calculatePercentageProgress() {
+        const { paused, counter } = this.state;
+        
+        if (paused && counter === 0) {
+            return 0;
+        }
+        
+        const remainingTime = this.props.time - this.state.counter;
+        return remainingTime / this.props.time
+    }
+    
+    togglePaused() {
+        this.setState({ paused: !this.state.paused });
+    }
 }
+
+Timer.propTypes = {
+    name: PropTypes.string,
+    time: PropTypes.number.isRequired
+};
+
+Timer.defaultProps = {
+    name: 'Timer'
+};
