6bf4a2f332137485e7583b1115af0ec928c8da14
Danilo Barros <danilobjr@gmail.com>
2016-06-05 20:01:28 -0300

    PageView component

---
+++ src/components/common/PageView.jsx
@@ -0,0 +1,18 @@
+import React, { Component, PropTypes } from 'react';
+import { FlexBox } from './FlexBox';
+
+export const PageView = (props) => 
+    <FlexBox 
+        className={`${props.className} h-allvh`}
+        column={!props.row}
+    >
+        {props.children}
+    </FlexBox>
+
+PageView.propTypes = {
+    row: PropTypes.bool
+};
+
+PageView.defaultProps = {
+    row: false
+};
