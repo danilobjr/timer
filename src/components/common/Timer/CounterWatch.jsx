6e05109c6a9226bd42b18f2ee4dfadd21514afa3
Danilo Barros <danilobjr@gmail.com>
2016-06-05 09:35:46 -0300

    showing unit values correctly

--- src/components/common/Timer/CounterWatch.jsx
+++ src/components/common/Timer/CounterWatch.jsx
@@ -1,13 +1,36 @@
-import React, { Component } from 'react';
+import React, { Component, PropTypes } from 'react';
+import { compose, time, replace, padLeft } from 'helpers';
 
 export class CounterWatch extends Component {
     render() {
+        const [ hours, minutes, seconds ] = this.getRemainingTime();
+        
         return (
             <div className="counter-watch">
-                <span className="unit">00</span>
-                <span className="unit active">21</span>
-                <span className="unit active">40</span>
+                <span className="unit">{this.padLeftWithZero(hours)}</span>
+                <span className="unit active">{this.padLeftWithZero(minutes)}</span>
+                <span className="unit active">{this.padLeftWithZero(seconds)}</span>
             </div>
         );
     }
+    
+    getRemainingTime() {
+        console.log(time(this.props.totalTime - this.props.currentTime));
+        return time(this.props.totalTime - this.props.currentTime);
+    }
+    
+    padLeftWithZero(value) {
+        console.log(compose(replace(' ', '0'), padLeft(2))(value));
+        return compose(replace(' ')('0'), padLeft(2))(value);
+    }
 }
+
+CounterWatch.propTypes = {
+    currentTime: PropTypes.number,
+    totalTime: PropTypes.number
+};
+
+CounterWatch.defaultProps = {
+    currentTime: 0,
+    totalTime: 0
+};
