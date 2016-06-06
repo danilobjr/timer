8b251f88e6931dec70d7fbc03ee7693d2e59c627
Danilo Barros <danilobjr@gmail.com>
2016-06-06 13:03:34 -0300

    PageView default style to get height of 100vh

--- src/components/common/PageView/PageView.jsx
+++ src/components/common/PageView/PageView.jsx
@@ -2,7 +2,7 @@ import React, { Component, PropTypes } from 'react';
 import { FlexBox } from 'components/common';
 
 export const PageView = (props) => 
-    <FlexBox className={`${props.className} h-allvh`} column={!props.row} >
+    <FlexBox className={`page-view ${props.className}`} column={!props.row} >
         {props.children}
     </FlexBox>
 
