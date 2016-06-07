648610b84886f08c71a3817caecc1a10d63afe81
Danilo Barros <danilobjr@gmail.com>
2016-06-07 06:28:32 -0300

    using 'components' alias to resolve path

--- src/index.jsx
+++ src/index.jsx
@@ -2,6 +2,6 @@ import './styles';
 
 import React from 'react';
 import ReactDOM from 'react-dom';
-import { App } from './components/App';
+import { App } from 'components/App';
 
 ReactDOM.render(<App />, document.getElementById('app'));
