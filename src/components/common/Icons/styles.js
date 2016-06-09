0faaef17314d3a90e4935ae746e52a118e03128f
Danilo Barros <danilobjr@gmail.com>
2016-06-08 21:22:16 -0300

    styles object to use inline in icon components

---
+++ src/components/common/Icons/styles.js
@@ -0,0 +1,16 @@
+export const iconStyles = {
+    base: {
+        stroke: 'black'
+    },
+    timerCommandButton: {
+        base: {
+            strokeWidth: 1.3
+        },
+        reset: {
+            fill: 'none',
+            strokeDasharray: '52px',
+            transform: 'rotateZ(-159deg)',
+            transformOrigin: '50%'
+        }
+    }
+};
