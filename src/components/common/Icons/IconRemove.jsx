3eba07c72f9e4af1f552988b75b023470d45230c
Danilo Barros <danilobjr@gmail.com>
2016-06-10 19:24:26 -0300

    removing inline svg attributes

--- src/components/common/Icons/IconRemove.jsx
+++ src/components/common/Icons/IconRemove.jsx
@@ -2,7 +2,7 @@ import * as React from 'react';
 import { Icon } from './Icon';
 
 export const IconRemove = () => 
-    <Icon name="remove" width={10} height={10} stroke-width="1">
+    <Icon name="remove" width={10} height={10}>
         <line x1="0" y1="0" x2="10" y2="10" />
         <line x1="10" y1="0" x2="0" y2="10" />
     </Icon>
