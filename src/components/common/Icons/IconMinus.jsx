1ff0e652ed73593346436c39d0a7c3ad6d9e45cf
Danilo Barros <danilobjr@gmail.com>
2016-06-10 15:51:17 -0300

    using crispEdges

--- src/components/common/Icons/IconMinus.jsx
+++ src/components/common/Icons/IconMinus.jsx
@@ -1,7 +1,8 @@
 import * as React from 'react';
 import { Icon } from './Icon';
+import { iconStyles } from './styles';
 
 export const IconMinus = () => 
     <Icon name="minus" width={10} height={10}>
-        <line x1="0" y1="5" x2="10" y2="5" />
+        <line x1="0" y1="5" x2="10" y2="5" style={iconStyles.crispEdges} />
     </Icon>
