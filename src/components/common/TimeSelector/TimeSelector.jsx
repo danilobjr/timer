422aa18bf6483d491fdb53d3aa88875568927f32
Danilo Barros <danilobjr@gmail.com>
2016-06-13 13:10:44 -0300

    removing state n using props

--- src/components/common/TimeSelector/TimeSelector.jsx
+++ src/components/common/TimeSelector/TimeSelector.jsx
@@ -1,4 +1,4 @@
-import React, { Component } from 'react';
+import React, { Component, PropTypes } from 'react';
 import { NumberSelector } from 'components/common';
 import { inc, dec } from 'helpers';
 import { createArrayOfNumbersOf, select } from './localHelpers';
@@ -7,64 +7,75 @@ export class TimeSelector extends Component {
     constructor(props) {
         super(props);
     
-        this.state = {
-            hours: 22,
-            minutes: 2,
-            seconds: 10
+        this.time = {
+            hours: props.hours,
+            minutes: props.minutes,
+            seconds: props.seconds
         };
     }
 
+    componentWillReceiveProps(newProps) {
+        const { hours, minutes, seconds } = newProps;
+        this.time = { hours, minutes, seconds };
+    }
+
     render() {
-        const { hours, minutes, seconds } = this.state;
+        const { hours, minutes, seconds } = this.time;
 
         return (
             <div className="time-selector">
                 <NumberSelector 
                     selected={hours} 
                     lastNumber={23} 
-                    onSelectNext={() => this.updateStateByProperty('hours', 23, inc)}
-                    onSelectPrevious={() => this.updateStateByProperty('hours', 23, dec)} 
-                    onSelectExactly={(number) => this.updateState('hours', number)}
+                    onSelectNext={() => this.updateDataByProperty('hours', 23, inc)}
+                    onSelectPrevious={() => this.updateDataByProperty('hours', 23, dec)} 
+                    onSelectExactly={(number) => this.updateData('hours', number)}
                 />
                 <NumberSelector 
                     selected={minutes} 
                     lastNumber={59} 
-                    onSelectNext={() => this.updateStateByProperty('minutes', 59, inc)}
-                    onSelectPrevious={() => this.updateStateByProperty('minutes', 59, dec)} 
-                    onSelectExactly={(number) => this.updateState('minutes', number)}
+                    onSelectNext={() => this.updateDataByProperty('minutes', 59, inc)}
+                    onSelectPrevious={() => this.updateDataByProperty('minutes', 59, dec)} 
+                    onSelectExactly={(number) => this.updateData('minutes', number)}
                 />
                 <NumberSelector 
                     selected={seconds} 
                     lastNumber={59} 
-                    onSelectNext={() => this.updateStateByProperty('seconds', 59, inc)}
-                    onSelectPrevious={() => this.updateStateByProperty('seconds', 59, dec)} 
-                    onSelectExactly={(number) => this.updateState('seconds', number)}
+                    onSelectNext={() => this.updateDataByProperty('seconds', 59, inc)}
+                    onSelectPrevious={() => this.updateDataByProperty('seconds', 59, dec)} 
+                    onSelectExactly={(number) => this.updateData('seconds', number)}
                 />
             </div>
         );
     }
 
-    updateState(property, number) {
-        console.log(property, number);
-        const newState = Object.assign(
+    updateData(property, number) {
+        const updatedData = Object.assign(
             {}, 
-            this.state, 
+            this.time, 
             { [property]: number }
         );
 
-        this.setState(newState);
+        this.props.onChange(updatedData);
     }
 
-    updateStateByProperty(property, lastNumber, operation) {
-        const selectedNumber = this.state[property];
+    updateDataByProperty(property, lastNumber, operation) {
+        const selectedNumber = this.time[property];
         const update = { [property]: select(createArrayOfNumbersOf(0)(lastNumber), selectedNumber, operation) };
 
-        const newState = Object.assign(
+        const updatedData = Object.assign(
             {}, 
-            this.state, 
+            this.time, 
             update
         );
 
-        this.setState(newState);
+        this.props.onChange(updatedData);
     }
 }
+
+TimeSelector.propTypes = {
+    hours: PropTypes.number,
+    minutes: PropTypes.number,
+    seconds: PropTypes.number,
+    onChange: PropTypes.func
+};
