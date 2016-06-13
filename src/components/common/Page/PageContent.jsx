931d69e7aa82afe8868fe7c8d121792f161a4e2d
Danilo Barros <danilobjr@gmail.com>
2016-06-13 09:23:25 -0300

    _PageContent default styles

--- src/components/common/Page/PageContent.jsx
+++ src/components/common/Page/PageContent.jsx
@@ -3,6 +3,6 @@ import classNames from 'classnames';
 import { FlexItem } from 'components/common';
 
 export const PageContent = (props) => 
-    <FlexItem {...props} className={classNames(props.className, 'h-overflowauto')}>
+    <FlexItem {...props} className={classNames('page-content', props.className, 'h-overflowauto')}>
         {props.children}
     </FlexItem>
