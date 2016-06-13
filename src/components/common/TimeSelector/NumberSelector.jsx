6f8839c055858c759b62eb1891a5dcf30fec0369
Danilo Barros <danilobjr@gmail.com>
2016-06-12 22:13:00 -0300

    correct arrangement of numbers and number selection

--- src/components/common/TimeSelector/NumberSelector.jsx
+++ src/components/common/TimeSelector/NumberSelector.jsx
@@ -4,20 +4,21 @@ import {
     compose, range, splitAt, reverse, map, flatten,
     padLeft, replace, inc
 } from 'helpers';
-
-const formatNumbers = map(compose(replace(' ')('0'), padLeft(2)));
+import { 
+    createArrayOfNumbersOf, formatNumbers, 
+    rearrangeNumbers, toListItem 
+} from './localHelpers';
 
 export class NumberSelector extends Component {
     constructor(props) {
         super(props);
     
-        const from = 0;
-        const until = this.props.fromZeroUntil;
+        const { selected, lastNumber } = this.props;
 
-        this.state = {
-            numbers: compose(flatten, reverse, splitAt(inc(until - NumberSelector.splitAt)), formatNumbers, range(from), inc)(until),
-            selected: 0
-        };
+        this.numbers = compose(
+            formatNumbers, 
+            createArrayOfNumbersOf(NumberSelector.listOfNumbersStartsWith)
+        )(lastNumber);
     }
 
     render() {
@@ -29,8 +30,8 @@ export class NumberSelector extends Component {
                             {this.renderNumbers()}
                         </ul>
                     </div>
-                    <button className="btn -up"><IconAngleUp /></button>
-                    <button className="btn -down"><IconAngleDown /></button>
+                    <button className="btn -up" onClick={this.selectPrevious.bind(this)}><IconAngleUp /></button>
+                    <button className="btn -down" onClick={this.selectNext.bind(this)}><IconAngleDown /></button>
                 </div>
                 <span className="label">hours</span>
             </div>
@@ -38,18 +39,34 @@ export class NumberSelector extends Component {
     }
 
     renderNumbers() {
-        const toListItem = map(n => <li key={n} className="number">{n}</li>);
-        return toListItem(this.state.numbers);
+        return compose(
+            toListItem,
+            rearrangeNumbers, 
+            splitAt(this.props.selected - NumberSelector.offset)
+        )(this.numbers);
+    }
+
+    selectNext() {
+        this.props.onSelectNext();
+    }
+
+    selectPrevious() {
+        this.props.onSelectPrevious();
     }
 }
 
 NumberSelector.propTypes = {
-    fromZeroUntil: PropTypes.number.isRequired,
-    label: PropTypes.string
+    label: PropTypes.string,
+    selected: PropTypes.number,
+    lastNumber: PropTypes.number.isRequired,
+    onSelectNext: PropTypes.func.isRequired,
+    onSelectPrevious: PropTypes.func.isRequired
 };
 
 NumberSelector.defaultProps = {
-    label: 'numbers'
+    label: 'numbers',
+    selected: 0
 };
 
-NumberSelector.splitAt = 5;
+NumberSelector.listOfNumbersStartsWith = 0;
+NumberSelector.offset = 5;
--- src/components/common/TimeSelector/TimeSelector.jsx
+++ src/components/common/TimeSelector/TimeSelector.jsx
@@ -1,14 +1,56 @@
 import React, { Component } from 'react';
 import { NumberSelector } from 'components/common';
+import { inc, dec } from 'helpers';
+import { createArrayOfNumbersOf, select } from './localHelpers';
 
 export class TimeSelector extends Component {
+    constructor(props) {
+        super(props);
+    
+        this.state = {
+            hours: 22,
+            minutes: 2,
+            seconds: 10
+        };
+    }
+
     render() {
+        const { hours, minutes, seconds } = this.state;
+
         return (
             <div className="time-selector">
-                <NumberSelector fromZeroUntil={23} />
-                <NumberSelector fromZeroUntil={59} />
-                <NumberSelector fromZeroUntil={59} />
+                <NumberSelector 
+                    selected={hours} 
+                    lastNumber={23} 
+                    onSelectNext={() => this.updateStateByProperty('hours', 23, inc)}
+                    onSelectPrevious={() => this.updateStateByProperty('hours', 23, dec)} 
+                />
+                <NumberSelector 
+                    selected={minutes} 
+                    lastNumber={59} 
+                    onSelectNext={() => this.updateStateByProperty('minutes', 59, inc)}
+                    onSelectPrevious={() => this.updateStateByProperty('minutes', 59, dec)} 
+                />
+                <NumberSelector 
+                    selected={seconds} 
+                    lastNumber={59} 
+                    onSelectNext={() => this.updateStateByProperty('seconds', 59, inc)}
+                    onSelectPrevious={() => this.updateStateByProperty('seconds', 59, dec)} 
+                />
             </div>
         );
+    }
+
+    updateStateByProperty(property, lastNumber, operation) {
+        const selectedNumber = this.state[property];
+        const update = { [property]: select(createArrayOfNumbersOf(0)(lastNumber), selectedNumber, operation) };
+
+        const newState = Object.assign(
+            {}, 
+            this.state, 
+            update
+        );
+
+        this.setState(newState);
     }
 }
