b979a45efa3f34d1b9615ebb4e0e417dfd54a452
Danilo Barros <danilobjr@gmail.com>
2016-06-13 13:11:14 -0300

    TimerNewPage state in sync

--- src/components/pages/TimerNewPage.jsx
+++ src/components/pages/TimerNewPage.jsx
@@ -1,25 +1,60 @@
 import React, { Component } from 'react';
 import { 
     CommandBar, CommandBarItem, NavigationBar, 
-    PageView, PageHeader, PageContent
+    PageView, PageHeader, PageContent,
     //FlexBox,
+    TimeSelector,
+    FieldText
 } from 'components/common';
 
 export class TimerNewPage extends Component {
+    constructor(props) {
+        super(props);
+    
+        this.state = {
+            name: 'Timer',
+            hours: 0,
+            minutes: 0,
+            seconds: 0
+        };
+    }
+
     render() {
+        const { name, hours, minutes, seconds } = this.state;
+
         return (
             <PageView className="timer-new-page">
                 <PageContent grow={1}>
                     <PageHeader>New Timer</PageHeader>
-                    {/*
-                    <FlexBox wrap justify='center'>
-                    </FlexBox>
-                    */}
+                    <div className="field">
+                        <TimeSelector
+                            hours={hours} 
+                            minutes={minutes} 
+                            seconds={seconds} 
+                            onChange={(value) => this.updateTime(value)} 
+                        />
+                    </div>
+
+                    <FieldText 
+                        label="Timer name" 
+                        defaultValue={name} 
+                        onChange={(value) => this.updateName(value)} 
+                    />
                 </PageContent>
                 <CommandBar>
                     <CommandBarItem icon="moreHorizontal" title="More" />
                 </CommandBar>
             </PageView>
         );
+    }
+
+    updateTime(value) {
+        const newState = Object.assign({}, this.state, value);
+        this.setState(newState);
+    }
+
+    updateName(value) {
+        const newState = Object.assign({}, this.state, { name: value });
+        this.setState(newState);
     }
 }
