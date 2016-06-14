e1d46f8aa76949b544672a54bee2c84cd7fc3da1
Danilo Barros <danilobjr@gmail.com>
2016-06-14 00:19:56 -0300

    including timerReducers

--- src/components/appReducers.js
+++ src/components/appReducers.js
@@ -3,13 +3,15 @@ import { combineReducers } from 'redux';
 import { routerReducer } from 'react-router-redux';
 import { countdownTimerReducers } from 'components/common/CountdownTimer';
 import { navigationBarReducers } from 'components/common/NavigationBar';
+import { timerReducers } from 'components/pages/Timer';
 
 const reducers = _.assign(
     {
         routing: routerReducer
     },
     countdownTimerReducers,
-    navigationBarReducers
+    navigationBarReducers,
+    timerReducers
 );
 
 export const appReducers = combineReducers(reducers);
