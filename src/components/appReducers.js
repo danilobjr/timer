b56d931575430f58ae816ba026c5873a149bbf42
Danilo Barros <danilobjr@gmail.com>
2016-06-14 17:08:16 -0300

    including titleBarReducers in redux

--- src/components/appReducers.js
+++ src/components/appReducers.js
@@ -3,6 +3,7 @@ import { combineReducers } from 'redux';
 import { routerReducer } from 'react-router-redux';
 import { countdownTimerReducers } from 'components/common/CountdownTimer';
 import { navigationBarReducers } from 'components/common/NavigationBar';
+import { titleBarReducers } from 'components/common/TitleBar';
 import { timerReducers } from 'components/pages/Timer';
 
 const reducers = _.assign(
@@ -11,7 +12,8 @@ const reducers = _.assign(
     },
     countdownTimerReducers,
     navigationBarReducers,
-    timerReducers
+    timerReducers,
+    titleBarReducers
 );
 
 export const appReducers = combineReducers(reducers);
