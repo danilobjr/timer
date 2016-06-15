3ae55de9c2cc7b6b59a222ffef8ecb5eb41a4147
Danilo Barros <danilobjr@gmail.com>
2016-06-14 22:06:23 -0300

    including configReducers in appReducers

--- src/components/appReducers.js
+++ src/components/appReducers.js
@@ -1,15 +1,17 @@
 import * as _ from 'lodash';
 import { combineReducers } from 'redux';
 import { routerReducer } from 'react-router-redux';
-import { countdownTimerReducers } from 'components/common/CountdownTimer';
-import { navigationBarReducers } from 'components/common/NavigationBar';
-import { titleBarReducers } from 'components/common/TitleBar';
-import { timerReducers } from 'components/pages/Timer';
+import * as configReducers from 'config/reducers';
+import * as countdownTimerReducers from 'components/common/CountdownTimer/reducers';
+import * as navigationBarReducers from 'components/common/NavigationBar/reducers';
+import * as titleBarReducers from 'components/common/TitleBar/reducers';
+import * as timerReducers from 'components/pages/Timer/reducers';
 
 const reducers = _.assign(
     {
         routing: routerReducer
     },
+    configReducers,
     countdownTimerReducers,
     navigationBarReducers,
     timerReducers,
