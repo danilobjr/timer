e1511b9b24a5ea717d95998aefe0fc1de4b93071
Danilo Barros <danilobjr@gmail.com>
2016-06-20 13:41:10 -0300

    basic chronometer tab

--- src/components/pages/Chronometer/ChronometerTab.jsx
+++ src/components/pages/Chronometer/ChronometerTab.jsx
@@ -1,12 +1,16 @@
 import React, { Component } from 'react';
-import { NavigationBar } from 'components/common';
+import { Tab, TabContent, FlexBox } from 'components/common';
 
 export class ChronometerTab extends Component {
     render() {
         return (
-            <div>
-                <div>Chronometer content</div>
-            </div>            
+            <Tab>
+                <TabContent>
+                    <FlexBox justify='center'>
+                        <h3>ChronometerTab</h3>
+                    </FlexBox>
+                </TabContent>
+            </Tab>
         );
     }
 }
