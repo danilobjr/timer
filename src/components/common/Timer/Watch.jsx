2ffc626a99818f16e736d137c6194b2e9fbfd4cd
Danilo Barros <danilobjr@gmail.com>
2016-06-21 17:28:25 -0300

    showing hundredths

--- src/components/common/Timer/Watch.jsx
+++ src/components/common/Timer/Watch.jsx
@@ -5,8 +5,7 @@ import { padLeftWithZero } from './localHelpers';
 
 export class Watch extends BaseComponent {
     render() {
-        const [ hours, minutes, seconds ] = time(this.props.time);
-        const oneSecond = milliseconds(0,0,1);
+        const [ hours, minutes, seconds, hundredths ] = time(this.props.time);
         const oneMinute = milliseconds(0,1);
         const oneHour = milliseconds(1);
         
@@ -18,9 +17,10 @@ export class Watch extends BaseComponent {
                 <span className={this.renderUnitCssClasses(oneMinute)} title="Minutes">
                     {padLeftWithZero(minutes)}
                 </span>
-                <span className={this.renderUnitCssClasses(oneSecond)} title="Seconds">
+                <span className="unit -active" title="Seconds">
                     {padLeftWithZero(seconds)}
                 </span>
+                {this.renderHundredths(hundredths)}
             </div>
         );
     }
@@ -43,13 +43,27 @@ export class Watch extends BaseComponent {
             { '-active': this.props.time >= value }
         );
     }
+
+    renderHundredths(hundredths) {
+        if (this.props.showHundredths) {
+            return (
+                <span className="unit -hundredths -active" title="Hundredths">
+                    {padLeftWithZero(hundredths)}
+                </span>
+            );
+        }
+
+        return null;
+    }
 }
 
 Watch.propTypes = {
     time: PropTypes.number,
+    showHundredths: PropTypes.bool,
     lightTheme: PropTypes.bool
 };
 
 Watch.defaultProps = {
-    time: 0
+    time: 0,
+    showHundredths: false
 };
