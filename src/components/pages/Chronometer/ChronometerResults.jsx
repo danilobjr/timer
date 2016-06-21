9a6181b45c84e261ccda3b8a27bd86178ab797b5
Danilo Barros <danilobjr@gmail.com>
2016-06-21 16:52:36 -0300

    refactoring ChronometerResults markup n creating styles

--- src/components/pages/Chronometer/ChronometerResults.jsx
+++ src/components/pages/Chronometer/ChronometerResults.jsx
@@ -1,32 +1,67 @@
 import React, { Component } from 'react';
+import { Watch } from 'components/common';
 
 export class ChronometerResults extends Component {
     render() {
         return (
             <div className="chronometer-results">
-                <h4>Partial laps</h4>
+                <header className="header">
+                    <h4 className="title">Laps</h4>
+                    <span>Partials</span>
+                </header>
                 <div className="scroller">
-                    <ul className="laps">
+                    <ol className="laps">
+                        <li className="lap">
+                            <div className="partial">
+                                <Watch time={6123} />
+                            </div>
+                            <div className="total">
+                                <Watch time={6123} />
+                            </div>
+                        </li>
+                        <li className="lap">
+                            <div className="partial">
+                                <Watch time={6123} />
+                            </div>
+                            <div className="total">
+                                <Watch time={6123} />
+                            </div>
+                        </li>
+                        <li className="lap">
+                            <div className="partial">
+                                <Watch time={6123} />
+                            </div>
+                            <div className="total">
+                                <Watch time={6123} />
+                            </div>
+                        </li>
+                        <li className="lap">
+                            <div className="partial">
+                                <Watch time={6123} />
+                            </div>
+                            <div className="total">
+                                <Watch time={6123} />
+                            </div>
+                        </li>
                         <li className="lap">
                             <div className="partial">
                                 <div className="watch">
                                     <span className="unit">00</span>
-                                    <span className="unit">01</span>
-                                    <span className="unit">21</span>
-                                    <span className="unit -small">32</span>
+                                    <span className="unit -active">01</span>
+                                    <span className="unit -active">21</span>
+                                    <span className="unit -active -small">32</span>
                                 </div>
                             </div>
-                            <p className="total">00:04:38,05</p>
+                            <div className="total">
+                                <div className="watch">
+                                    <span className="unit">00</span>
+                                    <span className="unit -active">01</span>
+                                    <span className="unit -active">21</span>
+                                    <span className="unit -active -small">32</span>
+                                </div>
+                            </div>
                         </li>
-                        <li className="lap">
-                            <p className="partial">00:00:01,20</p>
-                            <p className="total">00:04:38,05</p>
-                        </li>
-                        <li className="lap">
-                            <p className="partial">00:00:01,20</p>
-                            <p className="total">00:04:38,05</p>
-                        </li>
-                    </ul>
+                    </ol>
                 </div>
             </div>
         );
