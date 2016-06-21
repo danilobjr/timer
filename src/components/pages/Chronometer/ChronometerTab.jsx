b5c36c31200079f0fed187d84ca8785c120e925f
Danilo Barros <danilobjr@gmail.com>
2016-06-21 18:00:57 -0300

    reset button

--- src/components/pages/Chronometer/ChronometerTab.jsx
+++ src/components/pages/Chronometer/ChronometerTab.jsx
@@ -1,16 +1,59 @@
 import React, { Component } from 'react';
-import { FlexBox, Tab, TabContent, Timer } from 'components/common';
+import { connect } from 'react-redux';
+import { Tab, TabContent, Timer, WatchCommandButton } from 'components/common';
 import { ChronometerResults } from './ChronometerResults';
 
-export class ChronometerTab extends Component {
+class ChronometerTabComponent extends Component {
+    constructor(props) {
+        super(props);
+    
+        this.state = {
+            isResetButtonHide: true
+        };
+    }
+
     render() {
+        const { isLightThemeOn } = this.props;
+        const { isResetButtonHide } = this.state;
+
         return (
             <Tab className="chronometer-tab">
                 <TabContent>
-                    <Timer showHundredths />
+                    <Timer
+                        ref="timer" 
+                        showHundredths
+                        onPause={this.showResetButton.bind(this)}
+                        onReset={this.hideResetButton.bind(this)}
+                    >
+                        <WatchCommandButton
+                            icon="reset"
+                            title="Reset" 
+                            hideButton={isResetButtonHide}
+                            lightTheme={isLightThemeOn}
+                            onClick={this.resetTimer.bind(this)}
+                        />
+                    </Timer>
                     <ChronometerResults />
                 </TabContent>
             </Tab>
         );
     }
+
+    resetTimer() {
+        this.refs.timer.getWrappedInstance().reset();
+    }
+
+    showResetButton() {
+        this.setState({ isResetButtonHide: false });
+    }
+
+    hideResetButton() {
+        this.setState({ isResetButtonHide: true });
+    }
 }
+
+const mapStateToProps = (state) => ({
+    isLightThemeOn: state.isLightThemeOn
+})
+
+export const ChronometerTab = connect(mapStateToProps)(ChronometerTabComponent);
