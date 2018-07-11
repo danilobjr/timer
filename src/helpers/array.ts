54541e0a02b60a127ac88c14a0610fea1bf5fb14
Danilo Barros <danilobjr@gmail.com>
2018-07-11 19:43:09 -0300

    satisfy tslint

--- src/helpers/array.ts
+++ src/helpers/array.ts
@@ -1,40 +1,40 @@
-export const map = (func: Function) => (array: any[]) => array.reduce((accu, curr) => accu.concat(func(curr)), [])
+export const map = (func: Function) => (array: any[]) => array.reduce((accu, curr) => accu.concat(func(curr)), []);
 
-export const range = (from: number) => (to: number) => new Array(to).join(',').split(',').map((item, index) => index)
+export const range = (from: number) => (to: number) => new Array(to).join(',').split(',').map((item, index) => index);
 
-export const splitAt = (atIndex: number) => (array: any[]) => [array.slice(0, atIndex), array.slice(atIndex)]
+export const splitAt = (atIndex: number) => (array: any[]) => [array.slice(0, atIndex), array.slice(atIndex)];
 
-export const reverse = (array: any[]) => [...array].reverse()
+export const reverse = (array: any[]) => [...array].reverse();
 
-export const flatten = (array: any[]) => array.reduce((accu, curr) => accu.concat(curr), [])
+export const flatten = (array: any[]) => array.reduce((accu, curr) => accu.concat(curr), []);
 
-export const last = (array: any[]) => [...array][array.length - 1]
+export const last = (array: any[]) => [...array][array.length - 1];
 
 export const all = (predicate: (value: any, index: number, array: any[]) => boolean) => (array: any[]) =>
-  array.every(predicate)
+  array.every(predicate);
 
-export const join = (separator: string) => (array: any[]) => array.join(separator)
+export const join = (separator: string) => (array: any[]) => array.join(separator);
 
-export const head = (array: any[]) => [...array].shift()
+export const head = (array: any[]) => [...array].shift();
 
 export const prepend = (value: any) => (array: any) => [value, ...array];
 
 export const zipWith = (func: Function) => (firstArray: any[]) => (secondArray: any[]) => {
   const result = [];
 
-  for (var i = 0; i < firstArray.length; i++) {
+  for (let i = 0; i < firstArray.length; i++) {
     result.push(func(firstArray[i], secondArray[i]));
   }
 
   return result;
-}
+};
 
 export const aperture = (howMany: number) => (array: any[]) => {
   const result = [];
 
-  for (var i = 0; i <= array.length - howMany; i++) {
+  for (let i = 0; i <= array.length - howMany; i++) {
     result.push([...array].splice(i, howMany));
   }
 
   return result;
-}
+};
