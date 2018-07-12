e3f8c52eebb1ce9fa4f3fc815874663d17649dae
Danilo Barros <danilobjr@gmail.com>
2018-07-12 00:22:51 -0300

    remove invalid import

--- src/components/appReducers.ts
+++ src/components/appReducers.ts
@@ -3,14 +3,14 @@ import * as _ from 'lodash';
 import * as timerReducers from 'components/common/Timer/reducers';
 // import * as navigationTabsReducers from 'components/common/NavigationTabs/reducers';
 import * as titleBarReducers from 'components/common/TitleBar/reducers';
-import * as timerPageReducers from 'components/pages/timer/reducers';
+// import * as timerPageReducers from 'components/pages/timer/reducers';
 import { combineReducers } from 'redux';
 
 const reducers = _.assign(
   // configReducers,
   timerReducers,
   // navigationTabsReducers,
-  timerPageReducers,
+  // timerPageReducers,
   titleBarReducers,
 );
 
