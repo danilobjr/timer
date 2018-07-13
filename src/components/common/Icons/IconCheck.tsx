b9cb037d5a0f99e45e5738caf913e0fce6cd96ce
Danilo Barros <danilobjr@gmail.com>
2018-07-13 15:09:17 -0300

    fix svg icons

--- src/components/common/Icons/IconCheck.tsx
+++ src/components/common/Icons/IconCheck.tsx
@@ -1,17 +1,16 @@
 import * as React from 'react';
 import { SFC } from 'react';
 import { Icon } from './Icon';
-import { iconStyles } from './styles';
 
 const styles = {
   fill: 'none',
   strokeWidth: 1.2,
   transformOrigin: '100% 100%',
-  transform: 'rotateZ(45deg) translate(-3px, -1px)'
+  transform: 'rotateZ(45deg) translate(2px, 7px)',
 };
 
 export const IconCheck: SFC<{}> = () => (
   <Icon name="icon-check" width={20} height={16}>
     <polyline points="0,18 8,18 8,0" style={styles} />
   </Icon>
-)
+);
--- src/components/common/Icons/IconChecklist.tsx
+++ src/components/common/Icons/IconChecklist.tsx
@@ -7,12 +7,12 @@ const styles = {
   checkmark: {
     fill: 'none',
     transformOrigin: '100% 100%',
-    transform: 'rotateZ(45deg)'
+    transform: 'rotateZ(45deg)',
   },
   phrase: {
     strokeWidth: 1.3,
-    crispEdges: iconStyles.crispEdges
-  }
+    crispEdges: iconStyles.crispEdges,
+  },
 };
 
 export const IconChecklist: SFC<{}> = () => (
@@ -21,11 +21,11 @@ export const IconChecklist: SFC<{}> = () => (
       <polyline id="checkmark" points="0.5,5 3,5 3,1" style={styles.checkmark} />
       <line id="phrase" x1="1" y1="0.5" x2="14" y2="0.5" style={styles.phrase} />
     </defs>
-    <use xlinkHref="#checkmark" x="-1" y="-1.5" />
-    <use xlinkHref="#checkmark" x="-1" y="5" />
+    <use xlinkHref="#checkmark" x="-13" y="8" />
+    <use xlinkHref="#checkmark" x="-13" y="14" />
     <use xlinkHref="#phrase" x="6" y="2" />
     <use xlinkHref="#phrase" x="6" y="6" />
     <use xlinkHref="#phrase" x="6" y="10" />
     <use xlinkHref="#phrase" x="6" y="14" />
   </Icon>
-)
+);
