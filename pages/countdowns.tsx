1a3fb81f83f4e710db373d842560c0465bcbe575
Danilo Barros <danilobjr@gmail.com>
2018-07-13 18:59:24 -0300

    new todo

--- pages/countdowns.tsx
+++ pages/countdowns.tsx
@@ -9,6 +9,7 @@ import { CountdownsState, actions } from 'src/redux/modules/countdowns';
 
 type CountdownsPageProps = StateToProps & DispatchToProps;
 
+// TODO: remove all lightTheme reference from components and styles
 // TODO: set page/tab Title (in redux-saga or in _app using Helmet?)
 // TODO: set theme in <head> when expanded
 const CountdownsPage: SFC<CountdownsPageProps> = (props) => {
