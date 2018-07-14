672e29291714f9facb79e11477a018fae77756c2
Danilo Barros <danilobjr@gmail.com>
2018-07-14 13:45:30 -0300

    rename util function

--- src/components/common/TimeSelector/localUtils.ts
+++ src/components/common/TimeSelector/localUtils.ts
@@ -1,7 +1,7 @@
 import { compose, range, map, flatten, reverse, replace, padLeft, inc, dec, last } from 'utils';
 
 export const createArrayOfNumbers = (startsWith: number) => compose(range(startsWith), inc);
-export const formatNumbers = map(compose(replace(' ')('0'), padLeft(2)));
+export const padLeftWithZero = map(compose(replace(' ')('0'), padLeft(2)));
 export const rearrangeNumbers = compose(flatten, reverse);
 
 export const select = (array: any[], selected: any, operation: Function) => {
