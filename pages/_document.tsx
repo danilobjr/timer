58dacd1702d5b213e2ed4462930ed77a45e6b7c5
Danilo Barros <danilobjr@gmail.com>
2018-07-14 01:29:11 -0300

    satisfy tslint

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -1,5 +1,5 @@
 import * as React from 'react';
-import Document, { Head, Main, NextScript } from 'next/document'
+import Document, { Head, Main, NextScript } from 'next/document';
 
 export default class MyDocument extends Document {
   render() {
