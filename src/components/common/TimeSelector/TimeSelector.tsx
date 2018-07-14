f0be1c4551a270ed7ca54c8df324b716287694a3
Danilo Barros <danilobjr@gmail.com>
2018-07-14 11:38:15 -0300

    convert TimerSelect to a SFC

--- src/components/common/TimeSelector/TimeSelector.tsx
+++ src/components/common/TimeSelector/TimeSelector.tsx
@@ -1,5 +1,5 @@
 import * as React from 'react';
-import { Component } from 'react';
+import { SFC } from 'react';
 import { NumberSelector } from 'components/common';
 import { inc, dec } from 'utils';
 import { StringKeyValuePair } from 'models';
@@ -12,58 +12,54 @@ type TimeSelectorProps = {
   onChange?: (time: StringKeyValuePair) => void;
 };
 
-export class TimeSelector extends Component<TimeSelectorProps> {
-  render() {
-    const { hours, minutes, seconds } = this.props;
+export const TimeSelector: SFC<TimeSelectorProps> = (props) => (
+  <div className="time-selector">
+    <NumberSelector
+      label="hours"
+      selected={props.hours}
+      lastNumber={23}
+      onSelectNext={updateDataByProperty(props, 'hours', 23, inc)}
+      onSelectPrevious={updateDataByProperty(props, 'hours', 23, dec)}
+      onSelectExactly={updateData(props, 'hours')}
+    />
 
-    return (
-      <div className="time-selector">
-        <NumberSelector
-          label="hours"
-          selected={hours}
-          lastNumber={23}
-          onSelectNext={this.updateDataByProperty('hours', 23, inc)}
-          onSelectPrevious={this.updateDataByProperty('hours', 23, dec)}
-          onSelectExactly={this.updateData('hours')}
-        />
-        <NumberSelector
-          label="minutes"
-          selected={minutes}
-          lastNumber={59}
-          onSelectNext={this.updateDataByProperty('minutes', 59, inc)}
-          onSelectPrevious={this.updateDataByProperty('minutes', 59, dec)}
-          onSelectExactly={this.updateData('minutes')}
-        />
-        <NumberSelector
-          label="seconds"
-          selected={seconds}
-          lastNumber={59}
-          onSelectNext={this.updateDataByProperty('seconds', 59, inc)}
-          onSelectPrevious={this.updateDataByProperty('seconds', 59, dec)}
-          onSelectExactly={this.updateData('seconds')}
-        />
-      </div>
-    );
-  }
+    <NumberSelector
+      label="minutes"
+      selected={props.minutes}
+      lastNumber={59}
+      onSelectNext={updateDataByProperty(props, 'minutes', 59, inc)}
+      onSelectPrevious={updateDataByProperty(props, 'minutes', 59, dec)}
+      onSelectExactly={updateData(props, 'minutes')}
+    />
 
-  updateData = (property: string) => (number: number) => {
-    const updatedData = {
-      ...this.props,
-      [property]: number,
-    };
+    <NumberSelector
+      label="seconds"
+      selected={props.seconds}
+      lastNumber={59}
+      onSelectNext={updateDataByProperty(props, 'seconds', 59, inc)}
+      onSelectPrevious={updateDataByProperty(props, 'seconds', 59, dec)}
+      onSelectExactly={updateData(props, 'seconds')}
+    />
+  </div>
+);
 
-    this.props.onChange(updatedData);
-  }
+const updateData = (props: TimeSelectorProps, property: string) => (number: number) => {
+  const updatedData = {
+    ...props,
+    [property]: number,
+  };
 
-  updateDataByProperty = (property: string, lastNumber: number, operation: typeof inc) => () => {
-    const selectedNumber = (this.props as StringKeyValuePair)[property];
-    const update = { [property]: select(createArrayOfNumbersOf(0)(lastNumber), selectedNumber, operation) };
+  props.onChange(updatedData);
+};
 
-    const updatedData = {
-      ...this.props,
-      ...update,
-    };
+const updateDataByProperty = (props: TimeSelectorProps, property: string, lastNumber: number, operation: typeof inc) => () => {
+  const selectedNumber = (props as StringKeyValuePair)[property];
+  const update = { [property]: select(createArrayOfNumbersOf(0)(lastNumber), selectedNumber, operation) };
 
-    this.props.onChange(updatedData);
-  }
-}
+  const updatedData = {
+    ...props,
+    ...update,
+  };
+
+  props.onChange(updatedData);
+};
