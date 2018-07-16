ea917d21aec1132104d13c6d805a37059dda0ba9
Danilo Barros <danilobjr@gmail.com>
2018-07-16 11:54:11 -0300

    set meta tags in head

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -1,11 +1,22 @@
 import * as React from 'react';
 import Document, { Head, Main, NextScript } from 'next/document';
 
+// TODO: set head/theme color
+
 export default class MyDocument extends Document {
   render() {
     return (
       <html>
         <Head>
+          <meta charSet="utf-8" />
+          <meta httpEquiv="x-ua-compatible" content="ie=edge" />
+          <meta name="viewport" content="width=device-width, initial-scale=1.0, maximum-scale=1.0, user-scalable=no" />
+          {/* <meta name="theme-color" content="#006dff" /> */}
+
+          {/* <link rel="dns-prefetch" href="//fonts.googleapis.com" />
+          <link href="//fonts.googleapis.com/css?family=Roboto:300,400,500" rel="stylesheet" />
+          <link href="//fonts.googleapis.com/icon?family=Material+Icons" rel="stylesheet" /> */}
+          {/* <link rel="shortcut icon" href="/static/favicon.ico" /> */}
           <link rel="stylesheet" href="/_next/static/style.css" />
         </Head>
         <body>
