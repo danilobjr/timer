b3eaad49c8556c10533776aa8c69fdbc1b15ffd4
Danilo Barros <danilobjr@gmail.com>
2018-07-16 14:12:51 -0300

    remove unused components

--- src/components/common/Tab/Tab.tsx
+++
@@ -1,9 +0,0 @@
-import * as React from 'react';
-import * as classNames from 'classnames';
-import { HTMLProps, SFC } from 'react';
-
-interface TabProps extends HTMLProps<HTMLDivElement> { }
-
-export const Tab: SFC<TabProps> = ({ children, className }) => (
-  <div className={classNames('tab', className)}>{children}</div>
-)
