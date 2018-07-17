d0d01b350d6bc93787e1855e9f7a366d289006d7
Danilo Barros <danilobjr@gmail.com>
2018-07-17 17:00:06 -0300

    default label for log function

--- src/utils/functional.ts
+++ src/utils/functional.ts
@@ -1,7 +1,8 @@
 export const compose = (...functions: Function[]) => (value: any) =>
   functions.reduceRight((accu, curr) => curr(accu), value);
 
-export const log = (label: string) => (x: any) => {
+export const log = (label: string = 'LOG') => (x: any) => {
+  // tslint:disable-next-line:no-console
   console.log(label, x);
   return x;
 };
