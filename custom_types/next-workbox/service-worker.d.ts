697418251bbac9f8520b7d3adc814141c23b9cd5
Danilo Barros <danilobjr@gmail.com>
2018-07-19 12:58:07 -0300

    remove next-workbox custom types

--- custom_types/next-workbox/service-worker.d.ts
+++
@@ -1,9 +0,0 @@
-import { Component } from 'react';
-
-type ServiceWorkerProps = {
-  src: string;
-  scope?: string;
-  unregister?: boolean;
-}
-
-export default class ServiceWorker extends Component<ServiceWorkerProps> { }
