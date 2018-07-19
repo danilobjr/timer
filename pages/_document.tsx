543610a26195be3f5a74962a32c7eed63505cc63
Danilo Barros <danilobjr@gmail.com>
2018-07-18 21:02:58 -0300

    remove comments

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -11,12 +11,6 @@ export default class MyDocument extends Document {
         <Head>
           <meta charSet="utf-8" />
           <meta httpEquiv="x-ua-compatible" content="ie=edge" />
-          {/* <meta name="viewport" content="width=device-width, initial-scale=1.0, maximum-scale=1.0, user-scalable=no" /> */}
-          {/* <meta name="theme-color" content="#297acb" /> */}
-
-          {/* <link rel="dns-prefetch" href="//fonts.googleapis.com" />
-          <link href="//fonts.googleapis.com/css?family=Roboto:300,400,500" rel="stylesheet" />
-          <link href="//fonts.googleapis.com/icon?family=Material+Icons" rel="stylesheet" /> */}
           <link rel="shortcut icon" href="/static/favicon.ico" />
           <link rel="stylesheet" href="/_next/static/style.css" />
           <title>Timer</title>
