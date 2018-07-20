d5c653fd9545c630bdc8a1bbbee9ed5c06693c2a
Danilo Barros <danilobjr@gmail.com>
2018-07-20 13:26:45 -0300

    fix arrow left

--- src/components/common/Icons/IconArrowLeft.tsx
+++ src/components/common/Icons/IconArrowLeft.tsx
@@ -8,6 +8,6 @@ type IconArrowLeftProps = IconProps;
 export const IconArrowLeft: SFC<IconArrowLeftProps> = (props) => (
   <Icon name="icon-arrow-left" width={12} height={11} {...props}>
     <polyline points="5,0.5 0.5,5 5,10" fill="none" />
-    <line x1="0" y1="5" x2="12" y2="5" style={iconStyles.crispEdges} />
+    <line x1="1.2" y1="5" x2="12" y2="5" style={iconStyles.crispEdges} />
   </Icon>
 );
