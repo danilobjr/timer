fda3cd89f1cc406ae56a2051575ae698183ea92f
Danilo Barros <danilobjr@gmail.com>
2018-07-31 13:21:46 -0300

    refactor server.js

--- server.js
+++ server.js
@@ -30,37 +30,3 @@ app.prepare().then(() => {
     console.log(`> Ready on http://localhost:${port}`);
   });
 });
-
-// TODO: try use this instead of above
-// TODO: when navigate to /, rewrite url to /timer
-// TODO: if above item not work, try to redirect from index.tsx to timer.tsx using next/route
-// const express = require('express')
-// const next = require('next')
-
-// const dev = process.env.NODE_ENV !== 'production'
-// const app = next({ dev })
-// const handle = app.getRequestHandler()
-
-// app.prepare()
-// .then(() => {
-//   const server = express()
-
-//   server.get('/p/:id', (req, res) => {
-//     const actualPage = '/post'
-//     const queryParams = { title: req.params.id }
-//     app.render(req, res, actualPage, queryParams)
-//   })
-
-//   server.get('*', (req, res) => {
-//     return handle(req, res)
-//   })
-
-//   server.listen(3000, (err) => {
-//     if (err) throw err
-//     console.log('> Ready on http://localhost:3000')
-//   })
-// })
-// .catch((ex) => {
-//   console.error(ex.stack)
-//   process.exit(1)
-// })
