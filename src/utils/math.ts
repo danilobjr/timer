0b5a171d981c430c8c5d33d741a6d113691bebda
Danilo Barros <danilobjr@gmail.com>
2018-07-31 15:28:55 -0300

    satisfy tslint

--- src/utils/math.ts
+++ src/utils/math.ts
@@ -1,6 +1,6 @@
-export const inc = (x: number) => x + 1
+export const inc = (x: number) => x + 1;
 
-export const dec = (x: number) => x - 1
+export const dec = (x: number) => x - 1;
 
 export const add = (a: number) => (b: number) => a + b;
 
