d86a23f82f67326812db376c6682822d47d7e020
Danilo Barros <danilobjr@gmail.com>
2018-08-14 20:52:31 -0300

    fix styles for IconStop

--- src/components/common/Icons/IconStop.tsx
+++ src/components/common/Icons/IconStop.tsx
@@ -1,11 +1,18 @@
 import * as React from 'react';
 import { SFC } from 'react';
 import { Icon, IconProps } from './Icon';
+import { iconStyles } from './styles';
+
+const styles = {
+  ...iconStyles.base,
+  strokeWidth: 1.5,
+  fill: 'none',
+};
 
 type IconStopProps = Partial<IconProps>;
 
 export const IconStop: SFC<IconStopProps> = (props) => (
-  <Icon name="icon-play" width={18} height={18} {...props}>
-    <rect x="0.5" y="0.5" width="17" height="17" rx="2" ry="2" />
+  <Icon name="icon-stop" width={16} height={16} {...props}>
+    <rect x="0.5" y="0.5" width="15" height="15" style={styles} />
   </Icon>
 );
