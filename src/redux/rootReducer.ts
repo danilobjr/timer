c5b2d1255834760a4fc79a6a09a2e00bde668f63
Danilo Barros <danilobjr@gmail.com>
2018-08-14 18:04:48 -0300

    remove alarm redux module from rootReducer

--- src/redux/rootReducer.ts
+++ src/redux/rootReducer.ts
@@ -1,11 +1,9 @@
 import { combineReducers } from 'redux';
-import alarm from './modules/alarm';
 import chronometer from './modules/chronometer';
 import countdowns from './modules/countdowns';
 import toast from './modules/toast';
 
 export const rootReducer = combineReducers({
-  ...alarm,
   ...chronometer,
   ...countdowns,
   ...toast,
