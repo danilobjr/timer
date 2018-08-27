cfb1417d32d846694840b39843b13044d93f1eeb
Danilo Barros <danilobjr@gmail.com>
2018-08-27 17:36:46 -0300

    refactor(app): remove next manifest

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -1,19 +1,21 @@
 import * as React from 'react';
 import Document, { Head, Main, NextScript } from 'next/document';
-import Manifest from 'next-manifest/manifest';
 
 export default class MyDocument extends Document {
   render() {
     return (
       <html>
         <Head>
+          <title>Timer</title>
+
           <meta charSet="utf-8" />
           <meta httpEquiv="x-ua-compatible" content="ie=edge" />
+          <meta name="viewport" content="width=device-width" initial-scale="1" />
+          <meta name="theme-color" content="#297acb" />
+
           <link rel="shortcut icon" href="/static/favicon.ico" />
           <link rel="stylesheet" href="/_next/static/style.css" />
-          <title>Timer</title>
-          {/* TODO: remove this and its package */}
-          <Manifest themeColor="#297acb" />
+          <link rel="manifest" href="/static/manifest/manifest.json" />
         </Head>
         <body>
           <Main />
